bd81536aaf12e96dfb579e56087d12cdf056fd8e
c7d5a6 <godinfrog@gmail.com>
2021-04-06 22:50:42 +0300

    added versions

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,7 @@ if (environment.production) {
   enableProdMode();
 }
 
+console.log(`Starting Maŭlieńnie v${environment.appVersion}`);
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
